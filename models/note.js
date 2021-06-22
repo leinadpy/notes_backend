da6b91daaa733db322453ae028c59c823901cb7e
Daniel Ojeda <leinadpy@gmail.com>
2021-06-22 16:49:27 -0400

    starting notes 4.1

--- models/note.js
+++ models/note.js
@@ -1,25 +1,14 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minlength: 5,
-    required: true
+    required: true,
   },
-  date: { 
+  date: {
     type: Date,
-    required: true
+    required: true,
   },
   important: Boolean,
 })
@@ -29,7 +18,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Note', noteSchema)
